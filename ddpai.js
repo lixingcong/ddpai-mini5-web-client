185ff6071c6d3673e7d9e7af54eac1a02ca5f8d4
lixingcong <lixingcong@live.com>
2021-02-23 18:25:47 +0800

    add ddpai.js, download KML ok

--- ddpai.js
+++ ddpai.js
@@ -2,37 +2,50 @@
  * 从json中提取出data字段
  *
  * @param {string} inputJson 输入值，即API_GpsFileListReq的结果
- * @return {null|array} 当errcode字段为0时，返回data字段下的内容，否则返回null
+ * @return {array} 当errcode字段为0时，返回数组，每个元素是字典，键值对详见代码，否则返回Array()
  */
-function gpsFileListToArray(inputJson) {
+function API_GpsFileListReqToArray(inputJson) {
     var j = JSON.parse(inputJson);
-    if (0 == j.errcode) {
-        j = j.data;
-        return j;
-    }
-    return null;
-}
-
-/**
- * 从数组{[a,b],[b,c],[d,e]}中找出合适的时间跨度
- *
- * @param {number} fromTime 开始值timestamp，在timeArray中，合法的时间跨度的起始值应该 >= 本参数
- * @param {number} toTime 结束值timestamp，在timeArray中，合法的时间跨度的结束值应该 <= 本参数
- * @param {array} timeArray 数组{[a,b],[b,c],[d,e]}，每个元素[a,b]为[开始时间,结束时间]
- * @return {array} 一维数组，为timeArray中的下标，若找不到，则返回Array()
- */
-function findTimespan(fromTime, toTime, timeArray) {
     var ret = new Array();
-    var a0 = 0, a1 = 0;
-    timeArray.forEach(function (timestamps, index) {
-        a0 = timestamps[0];
-        a1 = timestamps[1];
-        if (fromTime <= a0 && a1 <= toTime)
-            ret.push(index);
-    });
+    if (0 == j.errcode) {
+        file = JSON.parse(j.data).file;
+        var lastStartTime = 0;
+        var startTime, endTime;
+        var f;
+        var lastArrayItem = undefined;
+        for (var i = 0; i < file.length; i++) {
+            f = file[i];
+            //console.log(f);
+            startTime = parseInt(f.starttime);
+            endTime = parseInt(f.endtime);
+
+            if (endTime !== lastStartTime) {
+                lastArrayItem = {
+                    'from': startTime,
+                    'to': endTime,
+                    'gpx': 0,
+                    'git': 0,
+                    'filename': new Array()
+                };
+                ret.push(lastArrayItem);
+            } else {
+                lastArrayItem['from'] = startTime;
+            }
+
+            filename = f.name;
+            if (filename.endsWith('git'))
+                lastArrayItem['git']++;
+            else if (filename.endsWith('gpx'))
+                lastArrayItem['gpx']++;
+
+            lastArrayItem['filename'].push(filename);
+            lastStartTime = startTime;
+        }
+    }
+
+    ret.forEach(function (i) { i['filename'].reverse(); });
     return ret;
 }
-
 
 /**
  * 将GPS数据中的dddmm.mmmmm转化为十进制的度数
@@ -60,7 +73,7 @@ function dddmmToDecimal(dddmm) {
  * @param {string} newline gpx回车换行符，应该是"\n"
  * @return {array} 字典，key为timestamp，value为timestamp时刻的位置信息，类型是字典，具体字段见代码
  */
-function gpxToPathArray(gpxFileContent, maxLineCount, newline) {
+function gpxToPathDict(gpxFileContent, maxLineCount, newline) {
     var ret = {};
     gpxes = gpxFileContent.split(newline);
     var actualLineCount = Math.min(maxLineCount, gpxes.length);
@@ -81,8 +94,8 @@ function gpxToPathArray(gpxFileContent, maxLineCount, newline) {
             latSgn = columns[4] === 'N' ? 1 : -1;
             lon = columns[5];
             lonSgn = columns[6] === 'E' ? 1 : -1;
-            knots = parseFloat(columns[7]);
-            heading = parseFloat(columns[8]);
+            // knots = parseFloat(columns[7]);
+            // heading = parseFloat(columns[8]);
             dateString = columns[9];
 
             // datetime
@@ -100,8 +113,8 @@ function gpxToPathArray(gpxFileContent, maxLineCount, newline) {
             var gps = {
                 'lat': dddmmToDecimal(lat) * latSgn,
                 'lon': dddmmToDecimal(lon) * lonSgn,
-                'speed': 1.852 * knots, // 速度单位：km/h
-                'heading': heading, // 航向单位：度
+                // 'speed': 1.852 * knots, // 速度单位：km/h
+                // 'heading': heading, // 航向单位：度
             };
             //console.log(gps);
             ret[timestamp] = gps;
@@ -111,11 +124,11 @@ function gpxToPathArray(gpxFileContent, maxLineCount, newline) {
             timeString = columns[1].substr(0, 6);
 
             if (timeString === lastTimeString) { // 一般情况是，GPGGA那一行，出现在GPRMC后
-                hdop = parseFloat(columns[8]);
+                // hdop = parseFloat(columns[8]);
                 altitude = parseFloat(columns[9]);
 
                 var gps = ret[timestamp]; // 取引用
-                gps['hdop'] = hdop; // 水平精度单位：米
+                // gps['hdop'] = hdop; // 水平精度单位：米
                 gps['altitude'] = altitude; // 海拔高度单位：米
             }
         }
@@ -124,15 +137,29 @@ function gpxToPathArray(gpxFileContent, maxLineCount, newline) {
     return ret;
 }
 
+function kmlHead(docName) {
+    return '\
+<?xml version="1.0" encoding="UTF-8"?>\n\
+<kml xmlns="http://earth.google.com/kml/2.2">\n\
+<Document><name>'+ docName + '</name>\n\
+<Placemark><name>path</name>\n\
+<LineString><altitudeMode>absolute</altitudeMode><coordinates>\n';
+}
+
+function kmlCoord(lat, lon, alt, precision) {
+    return lon.toFixed(precision) + ',' + lat.toFixed(precision) + ',' + alt.toFixed(1);
+}
+
+function kmlTail() {
+    return '\n</coordinates></LineString></Placemark></Document></kml>';
+}
+
 /* for test only 1 */
-// var testInputJson='{"errcode":0,"data":"{\\"num\\":17,\\"file\\":[{\\"index\\":\\"0\\",\\"type\\":\\"49\\",\\"starttime\\":\\"1613931135\\",\\"endtime\\":\\"1613931512\\",\\"name\\":\\"20210221181215_0377.git\\",\\"parentfile\\":\\"\\"},{\\"index\\":\\"1\\",\\"type\\":\\"49\\",\\"starttime\\":\\"1613930655\\",\\"endtime\\":\\"1613931135\\",\\"name\\":\\"20210221180415_0480.git\\",\\"parentfile\\":\\"\\"},{\\"index\\":\\"2\\",\\"type\\":\\"49\\",\\"starttime\\":\\"1613930175\\",\\"endtime\\":\\"1613930655\\",\\"name\\":\\"20210221175615_0480.git\\",\\"parentfile\\":\\"\\"},{\\"index\\":\\"3\\",\\"type\\":\\"49\\",\\"starttime\\":\\"1613929695\\",\\"endtime\\":\\"1613930175\\",\\"name\\":\\"20210221174815_0480.git\\",\\"parentfile\\":\\"\\"},{\\"index\\":\\"4\\",\\"type\\":\\"49\\",\\"starttime\\":\\"1613929215\\",\\"endtime\\":\\"1613929695\\",\\"name\\":\\"20210221174015_0480.git\\",\\"parentfile\\":\\"\\"},{\\"index\\":\\"5\\",\\"type\\":\\"49\\",\\"starttime\\":\\"1613928735\\",\\"endtime\\":\\"1613929215\\",\\"name\\":\\"20210221173215_0480.git\\",\\"parentfile\\":\\"\\"},{\\"index\\":\\"6\\",\\"type\\":\\"49\\",\\"starttime\\":\\"1613928255\\",\\"endtime\\":\\"1613928735\\",\\"name\\":\\"20210221172415_0480.git\\",\\"parentfile\\":\\"\\"},{\\"index\\":\\"7\\",\\"type\\":\\"49\\",\\"starttime\\":\\"1613927775\\",\\"endtime\\":\\"1613928255\\",\\"name\\":\\"20210221171615_0480.git\\",\\"parentfile\\":\\"\\"},{\\"index\\":\\"8\\",\\"type\\":\\"49\\",\\"starttime\\":\\"1613927295\\",\\"endtime\\":\\"1613927775\\",\\"name\\":\\"20210221170815_0480.git\\",\\"parentfile\\":\\"\\"},{\\"index\\":\\"9\\",\\"type\\":\\"49\\",\\"starttime\\":\\"1613926815\\",\\"endtime\\":\\"1613927295\\",\\"name\\":\\"20210221170015_0480.git\\",\\"parentfile\\":\\"\\"},{\\"index\\":\\"10\\",\\"type\\":\\"49\\",\\"starttime\\":\\"1613926388\\",\\"endtime\\":\\"1613926815\\",\\"name\\":\\"20210221165308_0427.git\\",\\"parentfile\\":\\"\\"},{\\"index\\":\\"11\\",\\"type\\":\\"48\\",\\"starttime\\":\\"1613931812\\",\\"endtime\\":\\"1613931872\\",\\"name\\":\\"20210221182332_0060.gpx\\",\\"parentfile\\":\\"\\"},{\\"index\\":\\"12\\",\\"type\\":\\"48\\",\\"starttime\\":\\"1613931752\\",\\"endtime\\":\\"1613931812\\",\\"name\\":\\"20210221182232_0060.gpx\\",\\"parentfile\\":\\"\\"},{\\"index\\":\\"13\\",\\"type\\":\\"48\\",\\"starttime\\":\\"1613931692\\",\\"endtime\\":\\"1613931752\\",\\"name\\":\\"20210221182132_0060.gpx\\",\\"parentfile\\":\\"\\"},{\\"index\\":\\"14\\",\\"type\\":\\"48\\",\\"starttime\\":\\"1613931632\\",\\"endtime\\":\\"1613931692\\",\\"name\\":\\"20210221182032_0060.gpx\\",\\"parentfile\\":\\"\\"},{\\"index\\":\\"15\\",\\"type\\":\\"48\\",\\"starttime\\":\\"1613931572\\",\\"endtime\\":\\"1613931632\\",\\"name\\":\\"20210221181932_0060.gpx\\",\\"parentfile\\":\\"\\"},{\\"index\\":\\"16\\",\\"type\\":\\"48\\",\\"starttime\\":\\"1613931512\\",\\"endtime\\":\\"1613931572\\",\\"name\\":\\"20210221181832_0060.gpx\\",\\"parentfile\\":\\"\\"}]}"}';
-// console.log(gpsFileListToArray(testInputJson));
+// var testInputJson = '{"errcode":0,"data":"{\\"num\\":17,\\"file\\":[{\\"index\\":\\"0\\",\\"type\\":\\"49\\",\\"starttime\\":\\"1613931135\\",\\"endtime\\":\\"1613931512\\",\\"name\\":\\"20210221181215_0377.git\\",\\"parentfile\\":\\"\\"},{\\"index\\":\\"1\\",\\"type\\":\\"49\\",\\"starttime\\":\\"1613930655\\",\\"endtime\\":\\"1613931135\\",\\"name\\":\\"20210221180415_0480.git\\",\\"parentfile\\":\\"\\"},{\\"index\\":\\"2\\",\\"type\\":\\"49\\",\\"starttime\\":\\"1613930175\\",\\"endtime\\":\\"1613930655\\",\\"name\\":\\"20210221175615_0480.git\\",\\"parentfile\\":\\"\\"},{\\"index\\":\\"3\\",\\"type\\":\\"49\\",\\"starttime\\":\\"1613929695\\",\\"endtime\\":\\"1613930175\\",\\"name\\":\\"20210221174815_0480.git\\",\\"parentfile\\":\\"\\"},{\\"index\\":\\"4\\",\\"type\\":\\"49\\",\\"starttime\\":\\"1613929215\\",\\"endtime\\":\\"1613929695\\",\\"name\\":\\"20210221174015_0480.git\\",\\"parentfile\\":\\"\\"},{\\"index\\":\\"5\\",\\"type\\":\\"49\\",\\"starttime\\":\\"1613928735\\",\\"endtime\\":\\"1613929215\\",\\"name\\":\\"20210221173215_0480.git\\",\\"parentfile\\":\\"\\"},{\\"index\\":\\"6\\",\\"type\\":\\"49\\",\\"starttime\\":\\"1613928255\\",\\"endtime\\":\\"1613928735\\",\\"name\\":\\"20210221172415_0480.git\\",\\"parentfile\\":\\"\\"},{\\"index\\":\\"7\\",\\"type\\":\\"49\\",\\"starttime\\":\\"1613927775\\",\\"endtime\\":\\"1613928255\\",\\"name\\":\\"20210221171615_0480.git\\",\\"parentfile\\":\\"\\"},{\\"index\\":\\"8\\",\\"type\\":\\"49\\",\\"starttime\\":\\"1613927295\\",\\"endtime\\":\\"1613927775\\",\\"name\\":\\"20210221170815_0480.git\\",\\"parentfile\\":\\"\\"},{\\"index\\":\\"9\\",\\"type\\":\\"49\\",\\"starttime\\":\\"1613926815\\",\\"endtime\\":\\"1613927295\\",\\"name\\":\\"20210221170015_0480.git\\",\\"parentfile\\":\\"\\"},{\\"index\\":\\"10\\",\\"type\\":\\"49\\",\\"starttime\\":\\"1613926388\\",\\"endtime\\":\\"1613926815\\",\\"name\\":\\"20210221165308_0427.git\\",\\"parentfile\\":\\"\\"},{\\"index\\":\\"11\\",\\"type\\":\\"48\\",\\"starttime\\":\\"1613931812\\",\\"endtime\\":\\"1613931872\\",\\"name\\":\\"20210221182332_0060.gpx\\",\\"parentfile\\":\\"\\"},{\\"index\\":\\"12\\",\\"type\\":\\"48\\",\\"starttime\\":\\"1613931752\\",\\"endtime\\":\\"1613931812\\",\\"name\\":\\"20210221182232_0060.gpx\\",\\"parentfile\\":\\"\\"},{\\"index\\":\\"13\\",\\"type\\":\\"48\\",\\"starttime\\":\\"1613931692\\",\\"endtime\\":\\"1613931752\\",\\"name\\":\\"20210221182132_0060.gpx\\",\\"parentfile\\":\\"\\"},{\\"index\\":\\"14\\",\\"type\\":\\"48\\",\\"starttime\\":\\"1613931632\\",\\"endtime\\":\\"1613931692\\",\\"name\\":\\"20210221182032_0060.gpx\\",\\"parentfile\\":\\"\\"},{\\"index\\":\\"15\\",\\"type\\":\\"48\\",\\"starttime\\":\\"1613931572\\",\\"endtime\\":\\"1613931632\\",\\"name\\":\\"20210221181932_0060.gpx\\",\\"parentfile\\":\\"\\"},{\\"index\\":\\"16\\",\\"type\\":\\"48\\",\\"starttime\\":\\"1613931512\\",\\"endtime\\":\\"1613931572\\",\\"name\\":\\"20210221181832_0060.gpx\\",\\"parentfile\\":\\"\\"}]}"}';
+// console.log(API_GpsFileListReqToArray(testInputJson));
 
 // var testTimeArray = [[1, 3], [3, 10], [10, 12]];
 // console.log(findTimespan(3, 11, testTimeArray));
-
-// var testDDDmm=['12345.6789', '2345.6789', '345.6789', '045.6789', '12345.0'];
-// testDDDmm.forEach(dddmm => {console.log(dddmmToDecimal(dddmm))});
 
 /* test gpx file
 var testGpxFileContent = '\
@@ -150,6 +177,10 @@ $GSENSORDATAFREQUENCY 50 v2.0\n\
 $GYRO,-0.569954,0.202751,0.271148\n\
 $GSENSOR,0.003565,0.006836,-1.582607\n\
 $GYRO,-0.550880,0.263787,0.778503';
-console.log(gpxToPathArray(testGpxFileContent, 6, '\n'));
+console.log(gpxToPathDict(testGpxFileContent, 6, '\n'));
 test gpx file */
+
+// console.log(kmlHead('xx33'));
+// console.log(kmlCoord(1.23456789, 2.23456789, 4.5, 4));
+// console.log(kmlTail());
 /* for test only 2 */
--- main.js
+++ main.js
@@ -1,77 +1,199 @@
-// Copyright (c) 2017 Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
-// This software is licensed under a MIT License
-// https://github.com/workhorsy/uncompress.js
+// global vars
+var gpsFileList = undefined;
+var serverHostUrl = 'http://files.local';
+// var serverHostUrl='http://193.168.0.1';
+var gpxContents = undefined;
+var gpxContentsExpectCount = 0;
+var gpxContentsCheckerTimer = undefined;
+var dateObj = new Date();
+var timestampOffset = dateObj.getTimezoneOffset() * 60000;
 
-
-function httpRequest(url, method, cb, responseType) {
-	let xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function() {
-		if (this.readyState === 4) {
-			cb(this.response, this.status);
-		} else if (this.readyState === 0) {
-			cb(null);
-		}
+Date.prototype.format = function (fmt) {
+	var o = {
+		"M+": this.getMonth() + 1, //月份
+		"d+": this.getDate(), //日
+		"h+": this.getHours(), //小時
+		"m+": this.getMinutes(), //分
+		"s+": this.getSeconds(), //秒
+		"q+": Math.floor((this.getMonth() + 3) / 3), //季度
+		"S": this.getMilliseconds() //毫秒
 	};
-	xhr.onerror = function() {
-		cb(null);
-	};
-	xhr.open(method, url, true);
-	xhr.timeout = 50000;
-	xhr.responseType = responseType;
-	xhr.send(null);
+	if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+	for (var k in o)
+		if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+	return fmt;
 }
 
-function onArchiveLoaded(archive) {
-	var entryList = document.getElementById('entryList');
-
-	archive.entries.forEach(function(entry) {
-		if (! entry.is_file) return;
-
-		entry.readData(function(data, err) {
-			var url = URL.createObjectURL(new Blob([data]));
-
-			entryList.innerHTML +=
-			'<a href="' + url + '" download="' + entry.name + '">' + entry.name + '</a>' + '<br />' +
-			'<b>Compressed Size:</b> ' + entry.size_compressed + '<br />' +
-			'<b>Uncompressed Size:</b> ' + entry.size_uncompressed + '<br />' +
-			'<b>Is File:</b> ' + entry.is_file + '<br />';
-
-			entryList.innerHTML += '<hr />';
-		});
-	});
+function timestampToString(ts, fmt) {
+	dateObj.setTime(ts * 1000 + timestampOffset);
+	return dateObj.format(fmt);
 }
 
-// Load all the archive formats
-loadArchiveFormats(['rar', 'zip', 'tar'], function() {
-	var button = document.getElementById('go');
-	button.innerHTML = "Download and extract";
-	button.disabled = false;
-});
+$('#fetch-gps-file-list').click(function () {
+	var entryList = $('#entryList');
+	entryList.empty();
+	gpsFileList = undefined;
+	$('#export-gps-merged').prop('disabled', true);
 
-document.getElementById('go').addEventListener('click', function() {
-	var entryList = document.getElementById('entryList');
-	entryList.innerHTML = '';
+	var url = serverHostUrl + '/g.php';
+	// var url = serverHostUrl+'/cmd.cgi?cmd=API_GpsFileListReq';
+	$.ajax({
+		type: 'GET',
+		url: url,
+		success: function (response) {
+			gpsFileList = API_GpsFileListReqToArray(response);
+			if (gpsFileList.length > 0) {
+				var fmt = 'MM月dd日 hh:mm';
+				var innerHtml = '<table>\n';
+				innerHtml += '<thead><tr><th>单选</th><th>多选</th><th>时间段</th><th>git</th><th>gpx</th></tr></thead>\n';
+				innerHtml += '<tbody>\n';
+				var row = 0;
+				gpsFileList.forEach(g => {
+					from = timestampToString(g['from'], fmt);
+					to = timestampToString(g['to'], fmt);
+					button = '<button class="btn-download-single" value="' + row + '">下载</button>';
+					checkBox = '<label><input type="checkbox" value="' + row + '" name="history-select">选择</label>';
+					innerHtml += '<tr><td>' + button + '</td><td>' + checkBox + '</td>' + '<td>' + from + '<br/>' + to + '</td>' + '<td>' + g['git'] + '</td>' + '<td>' + g['gpx'] + '</td></tr>\n';
+					row++;
+				});
+				innerHtml += '</tbody>\n';
+				innerHtml += '</table>\n';
+				entryList.html(innerHtml);
 
-	var url = document.getElementById('download_url').value;
-	httpRequest(url, 'GET', function(response, status) {
-		if (status === 200) {
-			var fileReader = new FileReader();
-			fileReader.onload = function() {
-				var array_buffer = this.result;
-
-				// Open the file as an archive
-				var archive = archiveOpenArrayBuffer("1.tar", '', array_buffer);
-				if (archive) {
-					console.info('Uncompressing ' + archive.archive_type + ' ...');
-					entryList.innerHTML = '';
-					onArchiveLoaded(archive);
-				} else {
-					entryList.innerHTML = '<span style="color: red">' + err + '</span>';
-				}
-			};
-			fileReader.readAsArrayBuffer(response);
-		} else {
+				$('.btn-download-single').click(function () {
+					var idxes = [$(this).val()];
+					downloadGpsPaths(idxes);
+				});
+				$('#export-gps-merged').prop('disabled', false);
+			} else {
+				entryList.html('GPS file list is empty!');
+			}
+		},
+		error: function () {
 			console.error("Failed to download file with status: ", status);
 		}
-	}, 'blob');
+	});
+});
+
+// 参数idxes为整数的数组
+function downloadGpsPaths(idxes) {
+	if (gpxContentsCheckerTimer !== undefined) {
+		clearInterval(gpxContentsCheckerTimer);
+		gpxContentsCheckerTimer = undefined;
+	}
+
+	gpxContents = new Array();
+	gpxContentsExpectCount = 0;
+	kmlFileList = $('#kmlFileList');
+	kmlFileList.empty();
+
+	idxes.forEach(idx => {
+		var g = gpsFileList[idx];
+		g['filename'].forEach(filename => {
+			var url = serverHostUrl + '/' + filename;
+			$.ajax({
+				type: 'GET',
+				url: url,
+				xhrFields: {
+					responseType: 'blob'
+				},
+				success: function (response) {
+					var fileReader = new FileReader();
+					if (filename.endsWith('.git')) {
+						// tar file
+						fileReader.onload = function () {
+							var array_buffer = this.result;
+							var archive = archiveOpenArrayBuffer("1.tar", '', array_buffer);
+							if (archive) {
+								var archiveEntryCount = archive.entries.length;
+								gpxContentsExpectCount += archiveEntryCount;
+								archive.entries.forEach(function (entry) {
+									if (!entry.is_file) return;;
+									entry.readData(function (data) {
+										var arrayBufReader = new FileReader();
+										arrayBufReader.onload = function () {
+											var sortKey = parseInt(entry.name.split('_')[0].substr(4));
+											gpxContents.push([sortKey, this.result]);
+										};
+										arrayBufReader.readAsText(new Blob([data]));
+									});
+								});
+							}
+						};
+						fileReader.readAsArrayBuffer(response);
+					} else {
+						gpxContentsExpectCount++;
+						fileReader.onload = function () {
+							var sortKey = parseInt(filename.split('_')[0].substr(4));
+							gpxContents.push([sortKey, this.result]);
+						};
+						fileReader.readAsText(response);
+					}
+				}
+			});
+		});
+	});
+
+	if (idxes.length > 0) {
+		gpxContentsCheckerTimer = setInterval(function () {
+			if (gpxContentsExpectCount <= gpxContents.length) {
+				gpxContents.sort(function (a, b) {
+					return a[0] > b[0] ? 1 : -1;
+				});
+				//console.log(gpxContents);
+				var pathDict = {};
+				gpxContents.forEach(gpx => {
+					pathDict = Object.assign({}, pathDict, gpxToPathDict(gpx[1], 150, '\n'));
+				});
+
+				var pathDictKeys = Object.keys(pathDict).sort();
+				if (pathDictKeys.length > 0) {
+					var kml = kmlHead('MyPath');
+					pathDictKeys.forEach(k => {
+						g = pathDict[k];
+						kml += kmlCoord(g['lat'], g['lon'], g['altitude'], 6) + ' ';
+					});
+					kml += kmlTail();
+					//console.log(kml);
+
+					var fmt = 'MM月dd日hh时mm分'
+					tsFrom = pathDictKeys[0];
+					tsTo = pathDictKeys[pathDictKeys.length - 1];
+					filename = '轨迹' + timestampToString(tsFrom, fmt) + '到' + timestampToString(tsTo, fmt) + '.kml';
+
+					var newLink = $('<a>', {
+						text: filename,
+						download: filename,
+						href: URL.createObjectURL(new Blob([kml]))
+					});
+					kmlFileList.append(newLink);
+				}
+
+				// clean up
+				clearInterval(gpxContentsCheckerTimer);
+				gpxContentsCheckerTimer = undefined;
+				gpxContents = undefined;
+			} else {
+				console.log('continue check, gpxContents.length=' + gpxContents.length + ', expect length=' + gpxContentsExpectCount);
+			}
+		}, 500);
+	}
+}
+
+$('#export-gps-merged').click(function () {
+	var checkedBoxes = $("input[name='history-select']:checked");
+	var idxes = new Array();
+
+	$.each(checkedBoxes, function () {
+		idxes.push(parseInt($(this).val()));
+	});
+
+	downloadGpsPaths(idxes);
+});
+
+// Load tar only
+loadArchiveFormats(['tar'], function () {
+	var button = $('#fetch-gps-file-list');
+	button.html("获取所有轨迹信息");
+	button.prop('disabled', false);
 });
